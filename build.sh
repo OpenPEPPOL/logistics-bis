c6e9a3edc959992a730fdc8998772116d82ccd4b
Rikard Larsson <rikard@beast.se>
2024-10-01 19:24:58 +0200

    New generation of example files

--- build.sh
+++ build.sh
@@ -74,16 +74,25 @@ docker run --rm -i -v $PROJECT:/src anskaffelser/validator:2.1.0 build -x -t -n 
 for sch in $PROJECT/rules/sch/*.sch; do
     docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/schematron:/target klakegg/schematron prepare /src/rules/sch/$(basename $sch) /target/$(basename $sch)
 done
-docker run --rm -i -v $PROJECT/target/site/files:/src alpine:3.6 rm -rf /src/Schematron.zip
-docker run --rm -i -v $PROJECT/target/schematron:/src -v $PROJECT/target/site/files:/target -w /src kramos/alpine-zip -r /target/Schematron.zip .
 
-# Example files
-docker run --rm -i -v $PROJECT/target/site/files:/src alpine:3.6 rm -rf /src/Examples.zip
-docker run --rm -i -v $PROJECT/rules/examples:/src -v $PROJECT/target/site/files:/target -w /src kramos/alpine-zip -r /target/Examples.zip .
-
-# Mapping files
-docker run --rm -i -v $PROJECT/target/site/files:/src alpine:3.6 rm -rf /src/Mapping.zip
-docker run --rm -i -v $PROJECT/rules/mapping:/src -v $PROJECT/target/site/files:/target -w /src kramos/alpine-zip -r /target/Mapping.zip .
+# Removing old zip files and creating new ones
+docker run --rm -i \
+  -v $PROJECT/target/site/files:/files \
+  -v $PROJECT/target/schematron:/schematron \
+  -v $PROJECT/rules/examples:/examples \
+  -v $PROJECT/rules/mapping:/mapping \
+  -w /tmp \
+  alpine:latest sh -c '
+    apk add --no-cache zip
+    rm -rf /files/Schematron.zip
+    cd /schematron && zip -r /files/Schematron.zip .
+    echo "Example files"
+    rm -rf /files/Examples.zip
+    cd /examples && zip -r /files/Examples.zip .
+    echo "Mapping files"
+    rm -rf /files/Mapping.zip
+    cd /mapping && zip -r /files/Mapping.zip .
+  '
 
 # Guides
 docker run --rm -i -v $PROJECT:/documents -v $PROJECT/target:/target difi/asciidoctor
