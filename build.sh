57e4310f9dec3768d874cd8aabbbee92b3b72839
RikardLarsson-BEAst <rikard@beast.se>
2026-03-06 08:59:24 +0100

    all ubl

--- build.sh
+++ build.sh
@@ -1,29 +1,6 @@
 #!/bin/sh
 
 PROJECT=$(dirname $(readlink -f "$0"))
-UBL_SYNTAX_FILE=${UBL_SYNTAX_FILE:-structure/syntax/ubl-pre-award-catalogue.xml}
-
-if [ -z "$UBL_OUTPUT_FILE" ]; then
-  UBL_TERM=$(sed -n 's:.*<Term>\([^<]*\)</Term>.*:\1:p' "$PROJECT/$UBL_SYNTAX_FILE" | head -n 1)
-  UBL_TERM_LOCAL=${UBL_TERM#*:}
-  UBL_IDENTIFIER=$(sed -n 's:.*<Property key="sch:identifier">\([^<]*\)</Property>.*:\1:p' "$PROJECT/$UBL_SYNTAX_FILE" | head -n 1)
-  UBL_TRANSACTION=${UBL_IDENTIFIER%%-*}
-
-  if [ -z "$UBL_TERM_LOCAL" ]; then
-    echo "Unable to derive UBL term from $UBL_SYNTAX_FILE"
-    exit 1
-  fi
-
-  if [ -z "$UBL_TRANSACTION" ]; then
-    UBL_TRANSACTION=generated
-  fi
-
-  UBL_OUTPUT_FILE=rules/examples/$UBL_TRANSACTION/${UBL_TERM_LOCAL}_full.xml
-fi
-
-mkdir -p "$PROJECT/$(dirname "$UBL_OUTPUT_FILE")"
-
-echo "Generating UBL example: input=$UBL_SYNTAX_FILE output=$UBL_OUTPUT_FILE"
 
 # Delete target folder if found
 if [ -e $PROJECT/target ]; then
@@ -49,12 +26,75 @@ for sch in $PROJECT/rules/sch/*.sch; do
     docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/schematron:/target klakegg/schematron prepare /src/rules/sch/$(basename $sch) /target/$(basename $sch)
 done
 
-# Generate full UBL example
-docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 \
-  -cp /saxon.jar net.sf.saxon.Transform \
-  -s:/src/$UBL_SYNTAX_FILE \
-  -xsl:/src/tools/create-ubl-example-from-syntax.xsl \
-  -o:/src/$UBL_OUTPUT_FILE
+# Generate full UBL examples
+echo "Generating UBL example: Expression Of Interest Request (T001)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-expression-of-interest-request.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T001/ExpressionOfInterestRequest_full.xml
+
+echo "Generating UBL example: Expression Of Interest Response (T002)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-expression-of-interest-response.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T002/ExpressionOfInterestResponse_full.xml
+
+echo "Generating UBL example: Tender Status Request (T003)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-tender-status-request.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T003/TenderStatusRequest_full.xml
+
+echo "Generating UBL example: Call For Tenders (T004)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-call-for-tenders.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T004/CallForTenders_full.xml
+
+echo "Generating UBL example: Tender (T005)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-submit-tender.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T005/Tender_full.xml
+
+echo "Generating UBL example: Tender Receipt (T006)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-tender-receipt.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T006/TenderReceipt_full.xml
+
+echo "Generating UBL example: Tendering Questions (T007)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-tendering-questions.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T007/TenderingQuestions_full.xml
+
+echo "Generating UBL example: Tendering Answers (T008)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-tendering-answers.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T008/TenderingAnswers_full.xml
+
+echo "Generating UBL example: Tender Clarification Request (T009)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-tender-clarification-request.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T009/TenderClarificationRequest_full.xml
+
+echo "Generating UBL example: Tender Clarification (T010)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-tender-clarification.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T010/TenderClarification_full.xml
+
+echo "Generating UBL example: Tender Withdrawal Request (T013)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-tender-withdrawal-request.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T013/TenderWithdrawalRequest_full.xml
+
+echo "Generating UBL example: Tender Withdrawal Response (T014)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-tender-withdrawal-response.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T014/TenderWithdrawalResponse_full.xml
+
+echo "Generating UBL example: Notice Publication Response (T016)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-publish-notice-response.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T016/NoticePublicationResponse_full.xml
+
+echo "Generating UBL example: Awarding Notification (T017)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-notify-awarding.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T017/AwardingNotification_full.xml
+
+echo "Generating UBL example: Tendering Message Response (T018)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-tendering-message-response.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T018/TenderingMessageResponse_full.xml
+
+echo "Generating UBL example: Qualification (T019)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-qualification.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T019/Qualification_full.xml
+
+echo "Generating UBL example: Qualification Receipt (T020)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-qualification-reception-confirmation.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T020/QualificationReceipt_full.xml
+
+echo "Generating UBL example: Unsubscribe From Procedure Request (T021)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-unsubscribe-from-procedure-request.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T021/UnsubscribeFromProcedureRequest_full.xml
+
+echo "Generating UBL example: Unsubscribe From Procedure Response (T022)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-unsubscribe-from-procedure-response.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T022/UnsubscribeFromProcedureResponse_full.xml
+
+echo "Generating UBL example: Qualification Response (T023)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-qualification-response.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T023/QualificationResponse_full.xml
+
+echo "Generating UBL example: Invitation To Tender (T024)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-invitation-to-tender.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T024/InvitationToTender_full.xml
+
+echo "Generating UBL example: Catalogue Request (T035)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-pre-award-catalogue-request.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T035/CatalogueRequest_full.xml
+
+echo "Generating UBL example: Catalogue (T036)"
+docker run --rm -i -v $PROJECT:/src --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Transform -s:/src/structure/syntax/ubl-pre-award-catalogue.xml -xsl:/src/tools/create-ubl-example-from-syntax.xsl -o:/src/rules/examples/T036/Catalogue_full.xml
 
 # Removing old zip files and creating new ones
 docker run --rm -i \
