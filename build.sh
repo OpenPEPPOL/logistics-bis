1c3c666e9aa7f9e3dcd306bc267f966b25f0b9aa
oscarglanzmann <oscar.glanzmann@ecrucial.se>
2026-06-10 09:08:11 +0200

    updated build script version to latest

--- build.sh
+++ build.sh
@@ -75,7 +75,7 @@ docker run --rm -i \
 
 # Testing validation rules 
 #docker run --rm -i -v $PROJECT:/src anskaffelser/validator:2.1.0 build -x -t -n eu.peppol.poacc.upgrade.v3 -a rules -target target/validator-test /src
-docker run --rm -i -v $PROJECT:/src phelger/vefa-validator:2.3.1 build -x -t -n eu.peppol.poacc.upgrade.v3 -a rules -target target/validator-test /src
+docker run --rm -i -v $PROJECT:/src phelger/vefa-validator:2.4.3 build -x -t -n eu.peppol.poacc.upgrade.v3 -a rules -target target/validator-test /src
 
 # Schematron
 for sch in $PROJECT/rules/sch/*.sch; do
