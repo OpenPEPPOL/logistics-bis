ae3f6ae2437151e9ca84907e051b18c18db7d4b8
oscarglanzmann <oscar.glanzmann@ecrucial.se>
2026-09-04 09:45:58 +0200

    updated import branch

--- sync_repos.sh
+++ sync_repos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script to sync specific directories and files from multiple Git repositories
 POACCREPO="https://github.com/OpenPEPPOL/poacc-upgrade-3.git"
-POACCBRANCH="2026-Q2-MR"
+POACCBRANCH="2026-Q2-QA2"
 
 # Function to clone and copy files
 sync_repo() {
